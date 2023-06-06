d4e6837745e3aacd928b8489309d9c0969414838
Ellie Parker <ellie@i-parker.com>
2023-06-05 22:11:45 -0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Typography>
+      It works! Yoo!
+    </Typography>
+  );
+}
+
+export default App;
